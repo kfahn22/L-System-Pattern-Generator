1d8467699ac89604b5c90ab751a2b32987dc41d2
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-08-10 17:25:43 -0400

    add more images

--- color_palette_from_url/shapes/knot.js
+++ color_palette_from_url/shapes/knot.js
@@ -4,7 +4,7 @@ class Knot {
   constructor(x, y, sc) {
     this.x = x;
     this.y = y;
-    this.sc = sc;
+    this.sc = sc/2;
     this.points = [];
   }
 
@@ -15,13 +15,6 @@ class Knot {
       this.points.push(createVector(x, y));
     }
   }
-  // addPoints() {
-  //   for (let theta = -2 * PI; theta < 2 * PI; theta += 0.1) {
-  //     let x = this.sc * (3 * sin(theta) + 2 * sin(3 * theta));
-  //     let y = this.sc * (cos(theta) + 2 * cos(3 * theta));
-  //     this.points.push(createVector(x, y));
-  //   }
-  // }
 
   show(angle) {
     push();
