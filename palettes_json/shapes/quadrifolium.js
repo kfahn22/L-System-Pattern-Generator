81fc5850a2a3c16d93a90f32a3daa2d3cdbeb9a6
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-08-11 10:38:29 -0400

    replace quadrifolium image

--- palettes_json/shapes/quadrifolium.js
+++ palettes_json/shapes/quadrifolium.js
@@ -10,7 +10,7 @@ class Quadrifolium {
   }
 
   addPoints() {
-    for (let theta = 0; theta < TWO_PI; theta += 0.1) {
+    for (let theta = 0; theta < TWO_PI; theta += 0.05) {
       let x = this.r * (2 * this.a * pow(sin(theta), 2) * cos(theta));
       let y = this.r * (2 * this.a * pow(cos(theta), 2) * sin(theta));
       this.points.push(createVector(x, y));
